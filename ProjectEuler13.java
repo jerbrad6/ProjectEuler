9599ea5edbfb855deeca32cb79c75a09f4f9aa43
Jeremy Bradford <jerbrad6@gmail.com>
2013-04-16 13:04:41 -0500

    Update ProjectEuler13.java

--- ProjectEuler13.java
+++ ProjectEuler13.java
@@ -2,6 +2,9 @@ import java.util.*;
 import java.io.*;
 
 //Problem 13: Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+//Soln strategy: input digits into arrays and process those individually.
+// Doesn't use bigInteger
 public class ProjectEuler13 {
 
 	/**
@@ -22,12 +25,12 @@ public class ProjectEuler13 {
 			}
 		}
 		
-		for (int i = 0; i < 100; i++) {
+		/*for (int i = 0; i < 100; i++) {
 		    for (int j = 0; j < 50; j++) {
 		        System.out.print(nums[i][j] + " ");
 		    }
 		    System.out.print("\n");
-		}
+		}*/
 		
 		
 		int [] sums= new int[100];
@@ -36,14 +39,14 @@ public class ProjectEuler13 {
 				sums[49-i]+=nums[j][i];
 			}
 		}
-		System.out.println(Arrays.toString(sums));
+		//System.out.println(Arrays.toString(sums));
 		for (int i=0; i<100; i++) {
 			if (sums[i]>9) {
 				sums[i+1]+=sums[i]/10;
 				sums[i]=sums[i]%10;
 			}
 		}
-		System.out.println(Arrays.toString(sums));
+		//System.out.println(Arrays.toString(sums));
 		int max=0;
 		for (int i=99; i>=0; i--) {
 			if (sums[i]!=0) {
