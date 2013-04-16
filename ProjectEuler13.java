a03a0a6e3c53163ae482e18c75b24bb5ed516fcf
Jeremy Bradford <jerbrad6@gmail.com>
2013-04-16 12:54:41 -0500

    Update ProjectEuler13.java

--- ProjectEuler13.java
+++ ProjectEuler13.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.io.*;
+
+//Problem 13: Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 public class ProjectEuler13 {
 
 	/**
